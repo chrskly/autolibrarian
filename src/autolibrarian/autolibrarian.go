4b19f4b756029ca0da61480f4793dc96b9d1cc12
Christian Kelly <ckelly@afilias.info>
2018-09-26 23:12:11 +0100

    Step through environments and check their Puppetfiles

--- src/autolibrarian/autolibrarian.go
+++ src/autolibrarian/autolibrarian.go
@@ -11,9 +11,8 @@ import (
 
 func CLI(module string, ref string) {
 
-    var c conf
-
     // Read in the configuration
+    var c conf
     c.LoadConfig()
     if c == (conf{}) {
         fmt.Fprintf(os.Stderr, "No config file found\n")
@@ -22,24 +21,27 @@ func CLI(module string, ref string) {
     fmt.Fprintf(os.Stdout, "conf : %v\n", c)
 
     // What environments do we have?
-    enumerateEnvironments(c.PuppetEnvironmentPath)
-
-    modules, err := puppetfile.Read("Puppetfile.short")
-    //contents, err := puppetfile.Read("Puppetfile")
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "ERROR command failed")
-        return
-    }
-
-    // Is this module in this Puppetfile
-    for _, m := range modules {
-        module_name := m.GetName()
-        fmt.Printf(">> %v\n", module_name)
-        m.ToString()
-        if module_name == module {
-            fmt.Sprintf("Updating module %s\n", module_name)
-            librarian.Update(module_name)
+    envs := enumerateEnvironments(c.PuppetEnvironmentPath)
+    for _, env := range envs {
+        fmt.Fprintf(os.Stdout, "Checking env %v\n", env)
+        puppet_file := c.PuppetEnvironmentPath + "/" + env + "/Puppetfile"
+        modules, err := puppetfile.Read(puppet_file)
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "ERROR command failed")
+            return
         }
+
+        // Is this module in this Puppetfile
+        for _, m := range modules {
+            module_name := m.GetName()
+            fmt.Printf(">> %v\n", module_name)
+            m.ToString()
+            if module_name == module {
+                fmt.Sprintf("Updating module %s\n", module_name)
+                librarian.Update(module_name)
+            }
+        }
+
     }
     //fmt.Fprintf(os.Stdout, contents)
 }
